e0c1754baaaf6c46dc4ed6cd82e8095eb5246430
Denis <denisosa911@gmail.com>
2018-04-17 00:24:22 +0300

    Updated Create method in CompaniesController, New view. Addet method Index and same_name view for Companies.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,15 +1,23 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
   def show
     @company = Company.find(params[:id])
   end
 
   def new
+    @company = Company.new
   end
 
   def create
     @company = Company.new(company_params)
-    @company.save
+    if @company.save
     redirect_to @company
+    else
+      render 'new'
+    end
   end
 
   private
